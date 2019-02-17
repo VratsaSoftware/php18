0a4e2ca16b0f86185ff379630f397adf68510951
Milena Tomova <kokolina1888@gmail.com>
2019-02-17 13:00:51 +0200

    sinc

--- 02_PHP&MySQL/04_CRUD/Demos/project/read.php
+++ 02_PHP&MySQL/04_CRUD/Demos/project/read.php
@@ -16,40 +16,40 @@ $result = mysqli_query($conn, $q_read);
 	<title>Read</title>
 </head>
 <body>
-	<?php 
+<?php 
 	if(mysqli_num_rows($result) > 0){
-		?>
-	<table border=1>
-		<tr>
-			<td>#</td>
-			<td>име</td>
-			<td>ISO CODE 2</td>
-			<td>ISO CODE 3</td>
-		</tr>
-		<?php
-	while($row = mysqli_fetch_assoc($result)){
-		?>
-		<tr>
-		<td>
-			<?= $row['country_id']?>
-		</td>
-		<td>
-			<?= $row['name']?>
-		</td>
-		<td>
-			<?= $row['iso_code_2'] ?>
-		</td>
-		<td>
-			<?= $row['iso_code_3'] ?>
-		</td>
-		</tr>
-		<?php 
-	}//end while
-	?>
-	</table>
+?>
+		<table border=1>
+			<tr>
+				<td>#</td>
+				<td>име</td>
+				<td>ISO CODE 2</td>
+				<td>ISO CODE 3</td>
+			</tr>
 	<?php
-	}//end if rows
+		while($row = mysqli_fetch_assoc($result)){
 	?>
+			<tr>
+				<td>
+					<?= $row['country_id']?>
+				</td>
+				<td>
+					<?= $row['name']?>
+				</td>
+				<td>
+					<?= $row['iso_code_2'] ?>
+				</td>
+				<td>
+					<?= $row['iso_code_3'] ?>
+				</td>
+			</tr>
+	<?php 
+		}//end while
+	?>
+		</table>
+<?php
+	}//end if rows
+?>
 	
 </body>
 </html>
