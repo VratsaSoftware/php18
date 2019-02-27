03e3707e88a8e691e729b6618865be591574ffeb
Milena Tomova <kokolina1888@gmail.com>
2019-02-27 20:43:06 +0200

    sinc

--- 02_PHP&MySQL/07_Sorting_and_Filtering/project/read.php
+++ 02_PHP&MySQL/07_Sorting_and_Filtering/project/read.php
@@ -87,49 +87,49 @@ $read_result = mysqli_query($conn, $read_query);
 				<div class="col-sm-2">
 					<input type="submit" name="filter_submit" class="btn btn-success" value="FILTER">
 				</div>
-
-			</form>
-		</div>	
-		<div class="row justify-content-md-center">			
-			<div class="col-sm-10">
-				<?php if(mysqli_num_rows($read_result) > 0){ ?>
-					<table class="table table-hover table-dark table-bordered">
-						<thead>
+			</div>
+		</form>
+	</div>	
+	<div class="row justify-content-md-center">			
+		<div class="col-sm-10">
+			<?php if(mysqli_num_rows($read_result) > 0){ ?>
+				<table class="table table-hover table-dark table-bordered">
+					<thead>
+						<tr>
+							<th>Product Name</th>
+							<th>Product Price</th>
+							<th>Product Qantity</th>
+							<th>Product Manufacturer</th>
+							<th>***</th>
+							<th>***</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php while($row = mysqli_fetch_assoc($read_result)){ ?>
 							<tr>
-								<th>Product Name</th>
-								<th>Product Price</th>
-								<th>Product Qantity</th>
-								<th>Product Manufacturer</th>
-								<th>***</th>
-								<th>***</th>
+								<td><?= $row['product_name']?></td>
+								<td><?= $row['price']?></td>
+								<td><?= $row['quantity']?></td>
+								<td><?= $row['manufacturer_name']?></td>
+								<td class="text-center">
+									<a href="update.php?product=<?= $row['product_id'] ?>" class="btn btn-warning">UPDATE</a>
+								</td>
+								<td class="text-center">
+									<a href="delete.php?product=<?= $row['product_id'] ?>" class="btn btn-danger">DELETE</a>
+								</td>
 							</tr>
-						</thead>
-						<tbody>
-							<?php while($row = mysqli_fetch_assoc($read_result)){ ?>
-								<tr>
-									<td><?= $row['product_name']?></td>
-									<td><?= $row['price']?></td>
-									<td><?= $row['quantity']?></td>
-									<td><?= $row['manufacturer_name']?></td>
-									<td class="text-center">
-										<a href="update.php?product=<?= $row['product_id'] ?>" class="btn btn-warning">UPDATE</a>
-									</td>
-									<td class="text-center">
-										<a href="delete.php?product=<?= $row['product_id'] ?>" class="btn btn-danger">DELETE</a>
-									</td>
-								</tr>
-							<?php } ?>
-						</tbody>
-					</table>
-				<?php } else {
-					echo "No result!";
-				}?>
-				<div class="row justify-content-md-center">
-					<a href="create.php" class="btn btn-secondary">Add new product</a>
-				</div>
+						<?php } ?>
+					</tbody>
+				</table>
+			<?php } else {
+				echo "No result!";
+			}?>
+			<div class="row justify-content-md-center">
+				<a href="create.php" class="btn btn-secondary">Add new product</a>
 			</div>
 		</div>
 	</div>
-	<?php 
-	include 'includes/footer.php';
-	?>
+</div>
+<?php 
+include 'includes/footer.php';
+?>
