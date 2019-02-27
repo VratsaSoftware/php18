81f3a5caad5fc35087c64f9cdf603357f7249f41
Milena Tomova <kokolina1888@gmail.com>
2019-02-27 20:28:55 +0200

    sinc

--- 02_PHP&MySQL/07_Sorting_and_Filtering/project/read.php
+++ 02_PHP&MySQL/07_Sorting_and_Filtering/project/read.php
@@ -26,6 +26,12 @@ if( isset($_GET['submit'])){
 	$read_query .= " ORDER BY ({$sort_by}) {$sort_direction} LIMIT {$limit}";
 }
 
+//FILTER BY 
+if( isset($_GET['filter_submit'])){
+	$manufacturer_id = $_GET['manufacturer_id'];
+	$read_query .= " WHERE p.manufacturer_id = {$manufacturer_id}";
+}
+
 $read_result = mysqli_query($conn, $read_query);
 
 ?>
@@ -57,46 +63,73 @@ $read_result = mysqli_query($conn, $read_query);
 			</div>
 		</form>
 	</div>
-	<div class="row justify-content-md-center">			
-		<div class="col-sm-10">
-			<?php if(mysqli_num_rows($read_result) > 0){ ?>
-				<table class="table table-hover table-dark table-bordered">
-					<thead>
-						<tr>
-							<th>Product Name</th>
-							<th>Product Price</th>
-							<th>Product Qantity</th>
-							<th>Product Manufacturer</th>
-							<th>***</th>
-							<th>***</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php while($row = mysqli_fetch_assoc($read_result)){ ?>
-							<tr>
-								<td><?= $row['product_name']?></td>
-								<td><?= $row['price']?></td>
-								<td><?= $row['quantity']?></td>
-								<td><?= $row['manufacturer_name']?></td>
-								<td class="text-center">
-									<a href="update.php?product=<?= $row['product_id'] ?>" class="btn btn-warning">UPDATE</a>
-								</td>
-								<td class="text-center">
-									<a href="delete.php?product=<?= $row['product_id'] ?>" class="btn btn-danger">DELETE</a>
-								</td>
-							</tr>
+	<?php 
+	$manufacturers_query = "SELECT * FROM manufacturer";
+	$manufacturers_result = mysqli_query($conn, $manufacturers_query);
+
+	?>
+	<div class="row justify-content-md-center mb-2">
+		<form action="" method="get">
+			<div class="form-row">				
+				<div class="col-sm-5">
+					<select class="custom-select" name="manufacturer_id">
+						<?php if(mysqli_num_rows($manufacturers_result) > 0){ ?>
+							
+							<?php while($row = mysqli_fetch_assoc($manufacturers_result)){ ?>
+
+								<option value="<?= $row['manufacturer_id'] ?>"><?= $row['name'] ?></option>
+
+							<?php } ?>
+
 						<?php } ?>
-					</tbody>
-				</table>
-			<?php } else {
-				echo "No result!";
-			}?>
-			<div class="row justify-content-md-center">
-				<a href="create.php" class="btn btn-secondary">Add new product</a>
+					</select>	
+				</div>
+				<div class="col-sm-2">
+					<input type="submit" name="filter_submit" class="btn btn-success" value="FILTER">
+				</div>
+
+			</form>
+		</div>	
+		<div class="row justify-content-md-center">			
+			<div class="col-sm-10">
+				<?php if(mysqli_num_rows($read_result) > 0){ ?>
+					<table class="table table-hover table-dark table-bordered">
+						<thead>
+							<tr>
+								<th>Product Name</th>
+								<th>Product Price</th>
+								<th>Product Qantity</th>
+								<th>Product Manufacturer</th>
+								<th>***</th>
+								<th>***</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php while($row = mysqli_fetch_assoc($read_result)){ ?>
+								<tr>
+									<td><?= $row['product_name']?></td>
+									<td><?= $row['price']?></td>
+									<td><?= $row['quantity']?></td>
+									<td><?= $row['manufacturer_name']?></td>
+									<td class="text-center">
+										<a href="update.php?product=<?= $row['product_id'] ?>" class="btn btn-warning">UPDATE</a>
+									</td>
+									<td class="text-center">
+										<a href="delete.php?product=<?= $row['product_id'] ?>" class="btn btn-danger">DELETE</a>
+									</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+				<?php } else {
+					echo "No result!";
+				}?>
+				<div class="row justify-content-md-center">
+					<a href="create.php" class="btn btn-secondary">Add new product</a>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<?php 
-include 'includes/footer.php';
-?>
+	<?php 
+	include 'includes/footer.php';
+	?>
