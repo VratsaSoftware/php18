1558898095d801d24c8ba6ab0cdbf7f66183e06b
Milena Tomova <kokolina1888@gmail.com>
2019-10-02 13:28:09 +0300

    sinc

---
+++ 04_PHP_OOP_and_MVC/07_l_intro/laravel_students/resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>       
+    <div class="container">
+        <ul>
+            <li>
+                <a href="<?php echo route('home') ?>">Home</a>
+            </li>
+            <li>
+                <a href="{{ route('homework') }}">Homework</a>
+            </li>
+            <li>
+                <a href="{{ route('profile') }}">Profile page</a>
+            </li>
+        </ul>
+        @yield('content')
+    </div>        
+    <script type="text/javascript" src=""></script>
+</body>
+</html>
