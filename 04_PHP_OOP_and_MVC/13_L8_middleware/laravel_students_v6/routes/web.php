6171a5fe81bc1fc4657a0b5dc1f693b7061f3803
Milena Tomova <kokolina1888@gmail.com>
2019-10-16 20:39:56 +0300

    sinc

--- 04_PHP_OOP_and_MVC/13_L8_middleware/laravel_students_v6/routes/web.php
+++ 04_PHP_OOP_and_MVC/13_L8_middleware/laravel_students_v6/routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function(){
+	Route::resource('courses', 'CoursesController')->only([
+    	'index', 'show'
+			]);
+	Route::group(['middleware' => 'role'], function(){
+		Route::resource('courses', 'CoursesController')->except([
+    	'index', 'show'
+			]);
+	});
+});
