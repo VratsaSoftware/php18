ec91bf6678d2f7d085797c8f1a5aa30a9f26d6c4
miletomova <mile.tomova@gmail.com>
2021-01-20 14:30:44 +0200

    Delete 01_PHP_1/L1_FT directory

--- 01_PHP_1/L1_FT/PHP Test - Alex/Task 01/task_1-2_Alex.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$year = $_POST['year'];
-
-if ($year % 4 == 0 && $year % 100 != 0) {
-	echo 'Leap year';
-} else {
-	echo 'Not a leap year';
-}
--- 01_PHP_1/L1_FT/test-1_Vesi_php/TASK_1/task_1_2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$year = $_POST ['year'];
-
-if ($year % 4 ==0 && $year % 100 != 0) {
-	echo 'leap year';
-} else {
-	echo 'not a leap year';
-}
